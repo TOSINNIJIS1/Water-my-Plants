6d109e17f6cce41be71fc50114e18a1290914fb9
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-10-04 13:20:36 -0400

    fift dashboard in progress

--- src/dashboard/fifth/fifthBoard.js
+++ src/dashboard/fifth/fifthBoard.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './fifthBoard.scss';
+import Feng from '../../feng.png';
+import Stat from '../../stat.png'
 
 export default function () {
 
 
     return (
-        <div>
+        <div class='fifthContainer'>
             <h4> Creating empathy </h4>
-            <p> Because I’m not the one experiencing these challanges, I needed to empathize with the planters who do. I defined the typical planter who needs a solution to these challenges. </p>
+            <p class='emp'> Because I’m not the one experiencing these challanges, I needed to empathize with the planters who do. I defined the typical planter who needs a solution to these challenges. </p>
 
-            <div>
+            <div class='feng'>
                 <h4> The feng shui enthusiast </h4>
-                <p> Loves adopting plants, but can’t get them organized to save her life. Her partner thinks she owns too many plants for their small apartment. She feels frustrated because often these plants have different needs and not many ways to tell them apart.   </p>
-                <img src='' alt='lady' />
+                
+                <div class='img'>
+                    <p> Loves adopting plants, but can’t get them organized to save her life. Her partner thinks she owns too many plants for their small apartment. She feels frustrated because often these plants have different needs and not many ways to tell them apart.   </p>
+                    <img src={Feng} alt='lady' />
+                </div>
             </div>
 
-            <div>
-                <p> I put myself in her shoes and stopped at every point in her journey to understand her needs, goals, and frustrations. </p>
-                <img src='' alt='stat' />
+            <div class='journey'>
+                <p> I put myself in her shoes and stopped at every point in her <bold class='bold'> journey </bold> to understand her needs, goals, and frustrations. </p>
+                <img src={Stat} alt='stat'  />
             </div>
 
             <div>
