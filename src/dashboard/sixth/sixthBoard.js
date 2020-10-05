55888e1d6ddd5aeff1dd4fc00cad72f161f847ef
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-10-05 14:35:05 -0400

    need to work on the 7 pictures alignment

--- src/dashboard/sixth/sixthBoard.js
+++ src/dashboard/sixth/sixthBoard.js
@@ -16,25 +16,39 @@ import DropletC from '../../assets/dropletC.png';
 export default function SixthBoard () {
 
     return (
-        <div>
-            <div class='diff'>
+        <div class='sixthContainer'>
+            <div class='idea'>
                 <h4> How might we differentiate between watered and unwatered plants? </h4>
                 <p> I had a couples ideas on how to denote a plant that needs water from a plant that’s not ready to be watered yet. I looked at how apps apply different states to selected and deselected items. I also considered symbols as a way to differentiate. In the end symbols are fitting in this scenario where a droplet of water could signify “dry” and plants without the droplet could mean watered already or not ready to be watered yet. </p>
-                <div>
-                    <img src={Group} alt='group' />
-                    <img src={Dashboard} alt='dashboard' />
-                    <img src={Succulents} alt='succulents' />
-                    <img src={Plants} alt='plants' />
-                    <img src={Snake} alt='snake' />
-                    <img src={Add} alt='add' />
-                    <img src={Settings} alt='setting' />
+                
+                <div class='images'>
+                    <div>
+                        <img src={Group} alt='group' />
+                        <img class='dash' src={Dashboard} alt='dashboard' />
+                        <img src={Succulents} alt='succulents' class='suc'/>
+
+                        <div class='one'>
+                            <img class='plants' src={Plants} alt='plants' />
+                            <img src={Snake} alt='snake' />
+                            <img class='add' src={Add} alt='add' />
+
+                        </div>
+                    </div>
+                        
+                    <div class='sett'>
+                        
+                        
+                        <img class='settings' src={Settings} alt='setting' />
+                    </div>
+                        
+
                 </div>
             </div>
 
             <div class='diff'>
                 <h4> Tracking watering </h4>
                 <p> Because indoor plant care is individual to each plants, users need to be able to see water history and set watering frequencies for them. </p>
-                <div>
+                <div class='track'>
                     <img src={Track} alt='track' />
                 </div>
 
@@ -43,13 +57,16 @@ export default function SixthBoard () {
                     <img src={Explore} alt='explore' />
                 </div>
 
-                <div>
+                <div class='solution'>
                     <h4> Final Solution </h4>
                     <p> I thought if a water droplet could signify dehydration, then perhaps you could tap that plant and mark it watered. But this is where I needed to get grounded with the goal of my users: they need to see all the plants that are dehydrated. </p>                    
 
-                    <img src={DropletA} alt='droplet' />
-                    <img src={DropletB} alt='droplet' />
-                    <img src={DropletC} alt='droplet' />
+                    <div class='drop'>
+                        <img src={DropletA} alt='droplet' class='dropA' />
+                        <img src={DropletB} alt='droplet' />
+                        <img src={DropletC} alt='droplet' />
+                    </div>
+                    
 
                 </div>
 
--- src/dashboard/userFlow/userflow.js
+++ src/dashboard/userFlow/userflow.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Wireframe from '../../WF.png';
 import './userFlow.scss';
 import SixthBoard from '../sixth/sixthBoard';
-
-
+import mediumZoom from 'medium-zoom'
 
 
 export default function UserFlow () {
 
+    mediumZoom('.zoom', {
+        margin: 50,
+    })
 
     return(
         <div class='uf'>
             <div class='w'>
-                <img src={Wireframe} alt='wireframe' class='wf' />
+                <img src={Wireframe} alt='wireframe' class='zoom' />                
             </div>
 
             <SixthBoard />
