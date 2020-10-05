b5ad17dbdda05d6836ba45f889451f8e1e253729
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-10-05 17:15:20 -0400

    done with the 7 image

--- src/dashboard/sixth/sixthBoard.js
+++ src/dashboard/sixth/sixthBoard.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './sixthBoard.scss';
-import Group from '../../assets/group.jpg';
-import Dashboard from '../../assets/dashboard.jpg';
-import Succulents from '../../assets/succulent.jpg';
-import Plants from '../../assets/plants.jpg';
-import Snake from '../../assets/snake.jpg';
-import Add from '../../assets/addPlant.jpg';
-import Settings from '../../assets/settings.jpg';
+import Image from '../../assets/image.jpg'
 import Track from '../../assets/track.png';
 import Explore from '../../assets/explore.png';
 import DropletA from '../../assets/dropletA.png';
@@ -22,13 +16,7 @@ export default function SixthBoard () {
                 <p> I had a couples ideas on how to denote a plant that needs water from a plant that’s not ready to be watered yet. I looked at how apps apply different states to selected and deselected items. I also considered symbols as a way to differentiate. In the end symbols are fitting in this scenario where a droplet of water could signify “dry” and plants without the droplet could mean watered already or not ready to be watered yet. </p>
                 
                 <div class='images'>
-                    <img class='group' src={Group} alt='group'  />
-                    <img class='dash' src={Dashboard} alt='dashboard' />
-                    <img class='suc' src={Succulents} alt='succulents' />
-                    <img class='plants' src={Plants} alt='plants' />
-                    <img class='snake' src={Snake} alt='snake' />
-                    <img class='add' src={Add} alt='add' />
-                    <img class='settings' src={Settings} alt='setting' />
+                    <img class='image' src={Image} alt='7image'  />
                     
                 </div>
             </div>
